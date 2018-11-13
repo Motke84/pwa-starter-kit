52aaf3d24400330f59823bd2738614e8266bb199
Moti Matatyahu <motke.mat@gmail.com>
2018-11-13 16:59:57 +0200

    Update my-view1.js
    add use flow-graph as inner component

--- src/components/my-view1.js
+++ src/components/my-view1.js
@@ -10,64 +10,31 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 import { html } from '@polymer/lit-element';
 import { PageViewElement } from './page-view-element.js';
-
+import { connect } from 'pwa-helpers/connect-mixin.js';
+import { store } from '../store.js';
 // These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
+import './graph.js';
 
-class MyView1 extends PageViewElement {
+import flowReducer from '../reducers/flowReducer';
+store.addReducers({
+  flowReducer
+});
 
-  firstUpdated(){
-      var graph = new joint.dia.Graph;
+class MyView1 extends  connect(store)(PageViewElement) {
 
-      var paper = new joint.dia.Paper({
-          el: this.shadowRoot.getElementById('myholder'),
-          model: graph,
-          width: 600,
-          height: 100,
-          gridSize: 1
-      });
+  firstUpdated(_changedProperties) {
 
-      var rect = new joint.shapes.standard.Rectangle();
-      rect.position(100, 30);
-      rect.resize(100, 40);
-      rect.attr({
-          body: {
-              fill: 'blue'
-          },
-          label: {
-              text: 'Hello',
-              fill: 'white'
-          }
-      });
-      rect.addTo(graph);
-
-      var rect2 = rect.clone();
-      rect2.translate(300, 0);
-      rect2.attr('label/text', 'World!');
-      rect2.addTo(graph);
-
-      var link = new joint.shapes.standard.Link();
-      link.source(rect);
-      link.target(rect2);
-      link.addTo(graph);
+    
   }
 
   render() {
 
+    
     return html`
       ${SharedStyles}
       <section>
-       <div id="myholder"></div>
-        <h2>Static page</h2>
-        <p>This is a text-only page.</p>
-        <p>It doesn't do anything other than display some static text.</p>
-      </section>
-      <section>
-        <h2>Welcome</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac nisi orci. Maecenas sollicitudin diam in diam efficitur cursus. Morbi sollicitudin in justo tincidunt placerat. Integer tincidunt elementum nisi, eu ornare dolor lacinia eget. Fusce pulvinar massa eget odio placerat, commodo molestie ipsum tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse porttitor id purus eu cursus. Suspendisse arcu nulla, mattis vel hendrerit et, malesuada a elit. Nam at diam ornare, aliquet est sed, malesuada metus. Cras nec enim vel nibh tincidunt euismod ut et enim. Etiam pharetra eros in sodales iaculis. Duis sagittis urna et cursus mollis. Cras tempor rutrum est. Praesent sollicitudin ligula at laoreet placerat. Praesent tortor dui, semper in sapien non, pharetra luctus turpis.</p>
-      </section>
-      <section>
-        <p>Vestibulum at est ex. Aenean id ligula id nibh dictum laoreet. Etiam non semper erat. Pellentesque eu justo rhoncus diam vulputate facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat metus ex, vel fringilla massa tincidunt sit amet. Nunc facilisis bibendum tristique. Mauris commodo, dolor vitae dapibus fermentum, odio nibh viverra lorem, eu cursus diam turpis et sapien. Nunc suscipit tortor a ligula tincidunt, id hendrerit tellus sollicitudin.</p>
+        <flow-graph></flow-graph>
       </section>
     `;
   }
