75bb42ec5a257beb61f91fabe4fe039148961edc
Moti Matatyahu <motke.mat@gmail.com>
2018-11-28 16:42:03 +0200

    Create shared-styles-su-all.js

---
+++ src/styles/shared-styles-su-all.js
@@ -0,0 +1,13 @@
+import { html } from '@polymer/lit-element';
+
+
+export const SharedStyleSuAll = html`
+<style>
+    
+  @import '../../../public/Semantic-UI-CSS-master/semantic.min.css';
+  @import '../../../public/joint.min.css';
+
+ 
+
+</style>
+`;
