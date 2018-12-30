63e81c43a03143407ec94018d5751d3d76034ce3
Moti Matatyahu <motke.mat@gmail.com>
2018-12-30 16:36:20 +0200

    Update flowActions.js

--- src/actions/flowActions.js
+++ src/actions/flowActions.js
@@ -3,40 +3,61 @@
 
 export const GET_FLOW_ITEMS = 'GET_FLOW_ITEMS';
 export const SAVE_FLOW_ITEMS = 'SAVE_FLOW_ITEMS';
+export const GOT_FLOW_ITEMS = 'GOT_FLOW_ITEMS';
+export const FLOW_ERROR = 'FLOW_ERROR';
 
+import {
+    showSnackbar
+} from '../actions/app.js';
 
 export const getAllFlowItems = (date) => (dispatch) => {
-    // Here you would normally get the data from the server. We're simulating
-    // that by dispatching an async action (that you would dispatch when you
-    // succesfully got the data back)
 
-    // You could reformat the data in the right format as well:
+
+    dispatch({
+        type: GET_FLOW_ITEMS,
+        date
+    });
+
     let flowItems;
     //https://moti-m-weather-api.herokuapp.com/getFlow
-    //http://localhost/SD.ICE.Admin.Dashboard/Flow/GetFlow
     axios({
-            url: `http://localhost/SD.ICE.Admin.Dashboard/Flow/GetFlow/${date}`,
-            method: 'GET',
-            data: date,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-                // "Content-Type": "application/json",
-                // "Accept": "application/json"
-            },
-            body: date,
-            compress: false
-        })
+        url: `http://localhost/SD.ICE.Admin.Dashboard/Flow/GetFlow/${date}`,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+            // "Content-Type": "application/json",
+            // "Accept": "application/json"
+        },
+        compress: false
+    })
         .then(function (response) {
-            console.log(response.data);
-            flowItems = response.data;
+            console.log(response);
+
+            let flowItems;
+            let action = GOT_FLOW_ITEMS;
+
+            if (response.data == "An unexpected error has occurred."){
+                dispatch(showSnackbar("Error In Server", true));
+                action = FLOW_ERROR;
+            }
+            else {
+                dispatch(showSnackbar("Success", false));
+                flowItems = response.data;
+            }
 
             dispatch({
-                type: GET_FLOW_ITEMS,
+                type: action,
                 flowItems
             });
         })
         .catch(function (error) {
             console.log(error);
+            dispatch(showSnackbar("Error In Server", true));
+
+            dispatch({
+                type: GOT_FLOW_ITEMS,
+                flowItems
+            });
         });
 
 };
@@ -66,13 +87,13 @@ export const saveFlowItems = (flowItems) => (dispatch) => {
         compress: false
     }).then(function (response) {
 
-            console.log(response.data);
+        console.log(response.data);
 
-            dispatch({
-                type: SAVE_FLOW_ITEMS,
-                flowItems
-            });
-        })
+        dispatch({
+            type: SAVE_FLOW_ITEMS,
+            flowItems
+        });
+    })
         .catch(function (error) {
             console.log(error);
         });
